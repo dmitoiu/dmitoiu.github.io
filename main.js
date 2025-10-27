04f13c474a5711c2c6e97a0cba3ac09514546144
Darie-Dragos Mitoiu <dragos_darie@hotmail.com>
2025-10-27 16:20:17 +0000

    Added Dark and Light Theme

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 class Online
 {
+  isDark = true;
   static run() {
     document.addEventListener("DOMContentLoaded", function () {
       try {
@@ -12,6 +13,7 @@ class Online
         let oDateEnd = new Date(Date.now());
         let weeks = Online.getWeeks(oDateStart, oDateEnd);
         sPublished.innerHTML = sPublishedInnerHTML + "&nbsp;" + "(" + weeks + " weeks ago" + ")";
+        Online.themeToggleHandle();
       } catch (exception) {
         console.error("Error", exception.message);
       }
@@ -24,6 +26,37 @@ class Online
     const difference = Math.abs(start_date.getTime() - end_date.getTime());
     return Math.floor(difference / oneWeek);
   }
+
+  static themeToggleHandle() {
+    let toggleWidget = document.getElementById("themeToggleWidget");
+    let toggleWidgetClassName = null;
+    let toggleWidgetContainer = null;
+    let themeContainer = document.getElementById("theme-container");
+    let htmlTag = document.getElementsByTagName("html")[0];
+    let themeClassNameString = null;
+    toggleWidget.addEventListener("click", function(e) {
+      toggleWidgetContainer = toggleWidget.children[0];
+      toggleWidgetClassName = toggleWidgetContainer.className;
+      let themeString = null;
+      let themeLabelObject = document.getElementById("theme-label");
+      if(toggleWidgetClassName.includes("dark")){
+        Online.isDark = true;
+        themeClassNameString = "light-theme";
+        themeString = `theme-toggle-widget-${Online.isDark ? "light" : "dark"}-mode`;
+        themeContainer.className = themeString;
+        themeLabelObject.innerText = "Light Mode";
+        htmlTag.className = themeClassNameString;
+      } else {
+        Online.isDark = false;
+        themeClassNameString = "dark-theme";
+        themeString = `theme-toggle-widget-${Online.isDark ? "light" : "dark"}-mode`;
+        themeContainer.className = themeString;
+        themeLabelObject.innerText = "Dark Mode";
+        htmlTag.className = themeClassNameString;
+      }
+    });
+  }
+
 }
 
 Online.run();
